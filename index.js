3ee2bb710e62b130912d8228b31dd8e9a2049c14
Ali Movahedi <ali_movahedi@aol.com>
2017-10-15 13:45:54 +0330

    add ws and switch

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let http = require('http'),
     httpProxy = require('http-proxy');
 
-let proxy = httpProxy.createProxyServer({});
+let proxy = httpProxy.createProxyServer({ ws: true });
 
 let server = http.createServer(function(req, res) {
-  proxy.web(req, res, { target: 'http://127.0.0.1:8010'});
+  switch(req.headers.host.split('.').reverse().slice(2).reverse().join('')) {
+    case 'foo':
+      break;
+    default:
+      proxy.web(req, res, { target: 'http://127.0.0.1:2080'});
+      break;
+  }
 });
 
 server.listen(80);
